6129f7c766cfcead3c4aa885bb186008062be042
Halida <halida.astatin@gmail.com>
2016-03-26 21:42:58 -0400

    - parameterizable tile size

--- crowdprogramming/game.js
+++ crowdprogramming/game.js
@@ -1,21 +1,47 @@
+/*parameters*/
+var board = [[0,0,1,0,0,0,1,0],
+		   [0,0,0,0,0,0,0,0],
+		   [0,0,0,1,1,1,0,0],
+		   [0,0,0,0,0,0,0,1],
+		   [1,1,1,0,0,0,0,0],
+		   [0,0,0,0,0,0,0,1],
+		   [0,0,0,0,0,0,1,0]];
+var boardHeight = 5;
+var boardWidth = 5;
+var tileSize = 100;
+var bonus = 0.000;
+var thisIsTheActualBonusValue = 0.000;
+var isAlive = false;
+var randomize = true;
+var miliseconds = new Date().getTime();
+var gameStart = new Date().getTime();
+var offset = Math.floor(miliseconds/1000)%board.length;
+var mode = 0;
+var idle = 0;
+var maxIdle = 0;
+var interaction = 0;
+
 function drawRow(row){
-	$(".board").prepend("<div class='row'><span></span><span></span><span></span><span></span><span></span></div>");
-	for (var j = 0; j < boardHeight; j++){
+	$(".board").prepend("<div class='row'></div>");
+	for (var j = 0; j < boardWidth; j++){
 		var charID = Math.floor((Math.random() * 4) + 1);
 		if(row[j] > 0){
-			$(".board .row").eq(0).find("span").eq(j).addClass("char char-"+charID);
+			$(".board .row").eq(0).append('<span class="char char-'+charID+'"></span>');
+		}
+		else{
+			$(".board .row").eq(0).append('<span></span>');
 		}
 	}
 }
 
 function checkCollide(){
-	var posX = parseInt($("#player").css("left"))/100;
+	var posX = parseInt($("#player").css("left"))/tileSize;
 	var xState = $(".board .row").eq(boardHeight-1).find("span").eq(posX).hasClass("char");
 	return xState;
 }
 
 function eatKid(){
-	var posX = parseInt($("#player").css("left"))/100;
+	var posX = parseInt($("#player").css("left"))/tileSize;
 	$(".board .row").eq(boardHeight-1).find("span").eq(posX).removeClass("char");
 	$("#player").addClass("nyam");
 	bonus+= 0.001;
@@ -44,28 +70,6 @@ function endGame(){
 		},3000);
 	});
 }
-
-/*game setup*/
-var board = [[0,0,1,0,0],
-		   [0,0,0,0,0],
-		   [0,0,0,1,1],
-		   [0,0,0,0,0],
-		   [1,1,1,0,0],
-		   [0,0,0,0,0],
-		   [0,0,0,0,0]];
-var boardHeight = 5;
-var boardWidth = 5;
-var bonus = 0.000;
-var thisIsTheActualBonusValue = 0.000;
-var isAlive = false;
-var randomize = true;
-var miliseconds = new Date().getTime();
-var gameStart = new Date().getTime();
-var offset = Math.floor(miliseconds/1000)%board.length;
-var mode = 0;
-var idle = 0;
-var maxIdle = 0;
-var interaction = 0;
 
 for (var i = board.length-1; i >= board.length - boardHeight; i--){
 	drawRow(board[i]);
@@ -115,12 +119,12 @@ $(document).keydown(function(e) {
 	idle = 0;
 	var position = parseInt($("#player").css("left"));
 	if(e.keyCode==37) {
-		position-=100;
+		position-=tileSize;
 	} else if(e.keyCode == 39) {
-		position+=100;
+		position+=tileSize;
 	}
-	if(position < 0) position = 400;
-	if(position > ((boardWidth - 1)*100)) position = 0;
+	if(position < 0) position = (boardWidth - 1) * tileSize;
+	if(position > ((boardWidth - 1)*tileSize)) position = 0;
 	$("#player").css("left",position+"px");
 	/*if(mode == 1){
 		if(checkCollide()) eatKid();
@@ -154,6 +158,7 @@ $(".mode-btn").click(function(){
 $(".start-btn").click(function(){
 	if($(this).hasClass("disabled")) return;
 	$("#intro").hide();
+	$("#player").css("left",Math.floor(boardWidth / 2)*tileSize+"px")
 	$("#game").fadeIn(400,function(){
 		isAlive = true;
 	});
