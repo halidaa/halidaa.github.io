f878d3870ecea45a7281ef7f11e65f9f2d2db5bb
Halida <halida.astatin@gmail.com>
2016-04-11 18:37:05 -0400

    fixed duplicate gup definitions

--- crowdprogramming/game-crowd.js
+++ crowdprogramming/game-crowd.js
@@ -129,15 +129,7 @@ function callServer(_data){
 	})
 }
 
-function gup( name, url ) {
-  if (!url) url = location.href;
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = "[\\?&]"+name+"=([^&#]*)";
-  var regex = new RegExp( regexS );
-  var results = regex.exec( url );
-  return results == null ? null : results[1];
-}
-var mediator = gup('mediator',document.location);
+var mediator = gup('mediator');
 if (mediator == null) mediator = "average";
 
 function getMediator(result){
